c35254a29fb39bcb03b275e423dde1100764fe7f
comihai <coca.mihai5@gmail.com>
2014-11-02 14:38:54 +0200

    -put .gitignore to right place

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import com.packets.cars.car;
-import com.packets.clients.client;
+import com.packets.carPack.CarClass;
+import com.packets.clientPack.ClientClass;
 
 /**
  * Created by mihai on 10/26/2014.
@@ -9,17 +9,17 @@ public class Main {
         try {
 
             //classes Instances
-            car c;
-            c = new car("volvo", "verde", 120, 45, 200, 78);
-            client cl;
-            cl = new client("Coca Mihai", 100);
+            CarClass c;
+            c = new CarClass("volvo", "verde", 120, 45, 200, 78);
+            ClientClass cl;
+            cl = new ClientClass("Coca Mihai", 100);
             cl.buy(c); //cumpar o masina prin asignarea valorii id-ului ei in variabila carId
             System.out.println("Numarul de inregistrare al masinii mele este : " + cl.getCarId());
 
-            car[] cars = new car[5];
+            CarClass[] cars = new CarClass[5];
             System.out.println(cars.length);
             for (int i = 0; i <= cars.length; i++) { //arunca eroarea fiindca am depasit lungimea vectorului
-                cars[i] = new car("dacia" + i, "visiniu", 23, 56, 122, 50);
+                cars[i] = new CarClass("dacia" + i, "visiniu", 23, 56, 122, 50);
                 System.out.println("Numarul de inmatriculare pt dacia" + i + " este : " + cars[i].getId());
             }
 
